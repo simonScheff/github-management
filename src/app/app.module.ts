4946dcbec414ddbcdbc9c271e8d4c09489a19bfb
simonScheff <shimsh16@gmail.com>
2020-05-08 12:20:53 +0300

    Create routing and add home page and call back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CallbackComponent } from './pages/callback/callback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule,
